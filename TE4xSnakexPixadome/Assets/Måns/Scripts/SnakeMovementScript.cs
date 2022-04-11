7e2b32f5600c03307436ec302c4e3922c6c8fb5b
Tofuzz <Luleamons@gmail.com>
2022-04-11 11:29:59 +0200

    2022-04-11
    byggdelar + sortering

---
+++ TE4xSnakexPixadome/Assets/Måns/Scripts/SnakeMovementScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SnakeMovementScript : MonoBehaviour
+{
+    int moveDistance;
+    Vector3 playerInputDirection;
+    Vector3 oldDirection;
+    float Timer;
+    float Time;
+    
+    public SnakeMovementScript()
+    {
+
+
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+
+
+    }
+}
