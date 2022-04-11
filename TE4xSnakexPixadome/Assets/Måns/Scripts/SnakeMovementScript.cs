1767a5fa3d07bacd304cfe79f476210b86df18d7
Tofuzz <Luleamons@gmail.com>
2022-04-11 23:36:06 +0200

    2022-04-11
    scripts

--- TE4xSnakexPixadome/Assets/Måns/Scripts/SnakeMovementScript.cs
+++ TE4xSnakexPixadome/Assets/Måns/Scripts/SnakeMovementScript.cs
@@ -6,31 +6,85 @@ using UnityEngine;
 
 public class SnakeMovementScript : MonoBehaviour
 {
-    int moveDistance;
-    Vector3 playerInputDirection;
-    Vector3 oldDirection;
-    float Timer;
-    float Time;
+    [SerializeField] GameObject snakeHead;
+    int direction;
+    Vector3 oldPosition;
+    float moveDistance;
+    float moveTimer;
+    float RotationDegree;
+    float time;
     
     public SnakeMovementScript()
     {
-
-
+        moveTimer = 1f;
+        moveDistance = 10f;
+        RotationDegree = 90f;
+        
     }
 
 
     void Start()
     {
+        direction = 0;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time = time + Time.deltaTime;
+        if (time >= moveTimer)
+        {
 
+            RotateYMinus();
+            time = 0f;
+        }
 
+    }
+
+    #region movement
+    void MoveXPlus()
+    {
+        snakeHead.transform.position = new Vector3
+            (snakeHead.transform.position.x + moveDistance, snakeHead.transform.position.y, snakeHead.transform.position.z);
 
 
     }
+    void MoveXMinus()
+    {
+        snakeHead.transform.position = new Vector3
+           (snakeHead.transform.position.x - moveDistance, snakeHead.transform.position.y, snakeHead.transform.position.z);
+
+
+    }
+    void MoveZplus()
+    {
+        snakeHead.transform.position = new Vector3
+           (snakeHead.transform.position.x, snakeHead.transform.position.y, snakeHead.transform.position.z + moveDistance);
+
+
+    }
+    void MoveZMinus()
+    {
+        snakeHead.transform.position = new Vector3
+           (snakeHead.transform.position.x, snakeHead.transform.position.y, snakeHead.transform.position.z - moveDistance);
+
+    }
+    #endregion
+
+    #region Rotation
+
+    void RotateYPlus()
+    {
+        transform.Rotate(snakeHead.transform.rotation.x, snakeHead.transform.rotation.y + RotationDegree, snakeHead.transform.rotation.z);
+
+    }
+    void RotateYMinus()
+    {
+        transform.Rotate(snakeHead.transform.rotation.x, snakeHead.transform.rotation.y - RotationDegree, snakeHead.transform.rotation.z);
+
+    }
+
+    #endregion
+
 }
