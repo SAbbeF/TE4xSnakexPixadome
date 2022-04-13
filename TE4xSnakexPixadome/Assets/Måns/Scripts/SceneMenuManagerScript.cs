f5bb40bab408dddb8216c5cd4859d06231b81e78
Tofuzz <Luleamons@gmail.com>
2022-04-13 13:19:57 +0200

    2022-04-13
    textmeshPro

---
+++ TE4xSnakexPixadome/Assets/Måns/Scripts/SceneMenuManagerScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMenuManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+
+    public void LoadGame()
+    {
+
+        SceneManager.LoadScene("MånsScene");
+
+    }
+
+
+    public void QuitGame()
+    {
+
+        Application.Quit();
+
+
+    }
+
+
+}
